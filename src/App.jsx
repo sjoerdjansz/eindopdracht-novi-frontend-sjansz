ffde3f0a9b5468d5327112ede6f59c661ccfae57
sjoerdjansz <sjoerdjansz@gmail.com>
2025-05-30 22:46:46 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+
+    return (
+        <>
+            <h1>Hello World</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, nostrum?</p>
+            <i className="fa-solid fa-user"></i>
+        </>
+    );
+}
+
+export default App;
