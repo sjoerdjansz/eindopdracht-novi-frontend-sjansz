49dc161f65f98b76618c6e683f3642271ce30d05
sjoerdjansz <sjoerdjansz@gmail.com>
2025-06-10 11:12:39 +0200

    (fix)minor fix with routes browserrouter placement

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 // CSS
 import "./App.css";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import { AppRoutes } from "./routes/AppRoutes.jsx";
 
 function App() {
-  return (
-    <Router>
-      <AppRoutes />
-    </Router>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
