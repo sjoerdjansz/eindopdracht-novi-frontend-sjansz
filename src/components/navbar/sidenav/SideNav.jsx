eaaaa84200553658e7cc139283b1874cbb615143
sjoerdjansz <sjoerdjansz@gmail.com>
2025-06-14 16:41:12 +0200

    (feat) side-nav styling updated and collapsing functionality through JS and CSS media queries

--- src/components/navbar/sidenav/SideNav.jsx
+++ src/components/navbar/sidenav/SideNav.jsx
@@ -4,75 +4,63 @@ import appLogo from "../../../assets/sweat-daddy-app-logo.svg";
 import { NavLink } from "react-router-dom";
 
 import {
-  faTableColumns,
-  faBook,
-  faDumbbell,
-  faUser,
-  faCalendarDay,
-  faPuzzlePiece,
+  faCircleChevronRight,
+  faCircleChevronLeft,
+  faArrowRightFromBracket,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+
+// nav links
+import { NAV_ITEMS } from "../../../data/navItems.js";
 
 export function SideNav() {
+  const [toggleNav, setToggleNav] = useState(true);
+
   return (
-    <nav className={styles["side-nav"]}>
-      <div className="side-nav__top-controls">
-        <div className={styles["logo-container"]}>
+    <nav
+      className={`${styles["side-nav"]} ${toggleNav ? styles["collapsed"] : ""}`}
+    >
+      <div className={styles["side-nav__top-controls"]}>
+        <div
+          className={`${styles["logo-container"]} ${toggleNav ? styles["collapsed"] : ""}`}
+        >
           <img src={appLogo} alt="sweat daddy app logo" />
           <p>Sweat Daddy</p>
         </div>
+        <span
+          className={styles["nav-toggle__wrapper"]}
+          onClick={() => setToggleNav(!toggleNav)}
+        >
+          <FontAwesomeIcon
+            icon={toggleNav ? faCircleChevronRight : faCircleChevronLeft}
+            className={styles["nav-toggle"]}
+          />
+        </span>
         <ul className={styles["side-nav__nav-links"]}>
-          <NavLink to="/dashboard">
-            <li className={styles["side-nav__link"]}>
-              <span>
-                <FontAwesomeIcon icon={faTableColumns} />
-              </span>
-              Dashboard
-            </li>
-          </NavLink>
-          <NavLink to="/exercise-library">
-            <li className={styles["side-nav__link"]}>
-              <span>
-                <FontAwesomeIcon icon={faBook} />
-              </span>
-              Exercise Library
-            </li>
-          </NavLink>
-          <NavLink to="/workouts">
-            <li className={styles["side-nav__link"]}>
-              <span>
-                <FontAwesomeIcon icon={faDumbbell} />
-              </span>
-              Workouts
-            </li>
-          </NavLink>
-          <NavLink to="/workout-builder">
-            <li className={styles["side-nav__link"]}>
-              <span>
-                <FontAwesomeIcon icon={faPuzzlePiece} />
-              </span>
-              Build Workout
-            </li>
-          </NavLink>
-          <NavLink to="/clients">
-            <li className={styles["side-nav__link"]}>
-              <span>
-                <FontAwesomeIcon icon={faUser} />
-              </span>
-              Clients
-            </li>
-          </NavLink>
-          <NavLink to="/agenda">
-            <li className={styles["side-nav__link"]}>
-              <span>
-                <FontAwesomeIcon icon={faCalendarDay} />
-              </span>
-              Agenda
-            </li>
-          </NavLink>
+          {NAV_ITEMS.map((navItem) => {
+            return (
+              <NavLink to={navItem.to} key={navItem.id}>
+                <li className={styles["side-nav__link"]}>
+                  <span className={styles["link__icon"]}>
+                    <FontAwesomeIcon icon={navItem.icon} />
+                  </span>
+                  <span className={styles["link__nav-label"]}>
+                    {navItem.label}
+                  </span>
+                </li>
+              </NavLink>
+            );
+          })}
         </ul>
       </div>
-      <NavLink to="/signup">Sign Out</NavLink>
+      <NavLink
+        to="/signup"
+        className={`${styles["side-nav__sign-out"]} ${toggleNav ? styles["collapsed"] : ""}`}
+      >
+        <FontAwesomeIcon icon={faArrowRightFromBracket} />
+        <span>Sign Out</span>
+      </NavLink>
     </nav>
   );
 }
